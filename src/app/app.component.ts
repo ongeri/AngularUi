e485369126f4550272664ecdb21622d10c6a723a
Japheth Ongeri <jg.ongeri@ubuni.solutions>
2018-06-21 18:21:38 +0300

    Added functionality for entering integers and sum and then calculating triplets that are less than sum.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,9 @@ export class AppComponent {
   title = 'Japheth Ongeri Tulaa Test UI';
   public reverseStringResult: string;
   public stringToReverse: string;
+  public tripletsSum: number;
+  public tripletsCounts: number;
+  public countTripletsIntegersString: string = '';
 
   constructor(private http: HttpClient) {
   }
@@ -34,5 +37,36 @@ export class AppComponent {
         console.log('Done reversing string', inputString);
       }
     );
+  }
+
+  countTriplets(tripletsSum: number) {
+    const integers = this.countTripletsIntegersString.split(',');
+    this.http.post(
+      environment.apiUrl + '/japheth/ongeri/tulaa/tripletSums',
+      {
+        distinctIntegers: integers,
+        sumValue: tripletsSum
+      },
+      {headers: new HttpHeaders({'Content-Type': 'application/json'})}
+    ).subscribe(
+      res => {
+        if (res) {
+          const data = res as any;
+          this.tripletsCounts = data.resultCount;
+        }
+      },
+      err => {
+        console.error(err);
+      },
+      () => {
+        console.log('Done counting triplets');
+      }
+    );
+  }
+
+  sanitizeIntegersList() {
+    this.tripletsCounts = null;
+    // this.countTripletsIntegersString = this.countTripletsIntegersString.replace(/\s/g, '');
+    this.countTripletsIntegersString = this.countTripletsIntegersString.replace(/[^\d,-]/g, '');
   }
 }
